927a636227ec331dcdd70075f06775455aa5659f
Fifa <den372115@gmail.com>
2022-11-16 14:25:20 +0700

    upload

--- myProject/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('products/', views.products, name='products'),
-    path('products/<int:product_id>', views.product, name='product'),
-    # path('cart/', views.cart, name='cart')
-]
--- myProject/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns=[
-    path('users/', include('django.contrib.auth.urls')),
-    path('users/register/', view=views.register, name='register' ),
-    path('users/dashboard/', view=views.dashboard, name='dashboard' ),
-    path('users/profile/', view=views.profile, name='profile' ),
-]
